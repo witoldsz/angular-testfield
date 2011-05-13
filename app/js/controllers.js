80c7a235d97d67e33995ee911c772ab66b60f47c
witoldsz <witoldsz@witold-desktop>
2011-05-13 23:18:01 +0200

    experimenting with tab widget

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,12 @@
 /* App Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+function TabsSampleCtrl() {
+  this.persons = [
+    {firstName: 'Stefan', lastName: 'Stefanowicz', age: 29},
+    {firstName: 'Iwan', lastName: 'Iwanowicz', age: 32},
+    {firstName: 'Ziut', lastName: 'Ziuto', age: 17}
+  ];
 }
-MyCtrl2.$inject = [];
+TabsSampleCtrl.$inject = [];
+
--- app/js/widgets.js
+++ app/js/widgets.js
@@ -1,1 +1,36 @@
 /* http://docs.angularjs.org/#!angular.widget */
+
+angular.widget('@my:tabs', function(arrayProperty, compileElement) {
+  var compiler = this;
+  var sequence = 0;
+  var template = $('.template', compileElement).hide();
+  compileElement.append("<ul></ul>");
+  compileElement.tabs();
+  return function(linkElement) {
+    var scope = this;
+    function createNewTab(i) {
+      var newId = 'tab' + sequence++;
+      var newTab = template.clone().show().attr('id', newId);
+      linkElement.append(newTab);
+      linkElement.tabs('add', '#'+newId, 'label');
+    } 
+    scope.$watch(arrayProperty+'.length', function(length, oldLength) {
+      var last = length - 1;
+      var i;
+      if (length > oldLength) {
+        for (i = oldLength; i < length; i++) {
+          createNewTab(i);
+        }
+      } else if (length < oldLength) {
+        for (i = length; i > oldLength; i++) {
+          linkElement.tabs('remove', last);
+        }
+      } else {
+        for (i=0; i<length; i++) {
+          createNewTab(i);
+        }
+      }
+      alert(value);
+    });
+  };
+});
