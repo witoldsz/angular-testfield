9e07781216525107a695786141889e02334d7ad0
witoldsz <witoldsz@witold-desktop>
2011-05-15 13:44:00 +0200

    add/remove buttons and functions

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,19 @@
 
 
 function TabsSampleCtrl() {
+  var self = this;
   this.persons = [
     {firstName: 'Stefan', lastName: 'Stefanowicz', age: 29},
     {firstName: 'Iwan', lastName: 'Iwanowicz', age: 32},
     {firstName: 'Ziut', lastName: 'Ziuto', age: 17}
   ];
+  this.addPerson = function() {
+    var rnd = Math.floor(Math.random()*101);
+    self.persons.push({firstName:'firstName'+rnd, lastName:'lastName'+rnd});
+  }
+  this.delPerson = function() {
+    self.persons.pop();
+  }
 }
 TabsSampleCtrl.$inject = [];
 
